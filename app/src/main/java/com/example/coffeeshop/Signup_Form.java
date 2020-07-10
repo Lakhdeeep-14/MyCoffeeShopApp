f2648da3a0d98bda594b6aa221965c2cc7a2fd82
Lakhdeep-14 <lakhdeepchauhan88@gmail.com>
2020-07-10 16:06:40 -0400

    Coffee Shop

--- app/src/main/java/com/example/coffeeshop/Signup_Form.java
+++ app/src/main/java/com/example/coffeeshop/Signup_Form.java
@@ -10,5 +10,6 @@ public class Signup_Form extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup__form);
+        getSupportActionBar().setTitle("Signup");
     }
 }
